18f24a045756b2d7adb1e58ae3b24b8a64658c38
proller <proller@github.com>
2013-01-27 01:23:40 +0400

    fixes

--- finite.lua
+++ finite.lua
@@ -80,7 +80,7 @@ minetest.register_abm({
 		dPrint("")
 		dPrint("Waterplus [finite] - Calculating for "..node_id.." at "..pos.x..","..pos.y..","..pos.z)
 
-        local upc = {x=pos.x, y=pos.y-1, z=pos.z}
+        local upc = {x=pos.x, y=pos.y+1, z=pos.z}
         -- recieve pressure from up
         local pressure = 0
         if minetest.env:get_node(upc).name == waterplus.finite_water_max_name or minetest.env:get_node(upc).name == "default:water_source" then
@@ -89,48 +89,51 @@ minetest.register_abm({
         end
 		dPrint("Waterplus [finite] - Calculating for "..node_id.." at "..pos.x..","..pos.y..","..pos.z..' press='..pressure)
 		
-		local target = {x=pos.x,y=pos.y,z=pos.z}
-		target.y=target.y-1
+		local target = {x=pos.x,y=pos.y-1,z=pos.z}
 		dPrint(target.x..","..target.z)
 		--if performDrop(pos,target) then return end
 		if performDrop(pos,target) then 
             if minetest.env:get_node(upc).name == "default:water_source" then
-		        minetest.env:set_node(upc,{name = "waterplus:finite_"..waterplus.finite_water_max_id})
+                minetest.env:set_node(upc,{name = waterplus.finite_water_max_name})
             end
             pos=target 
         end
 
-    	local source_name = minetest.env:get_node(pos).name
-	    local source_id = getNumberFromName(source_name) or 0 
-    	local coords = {
+        local source_name = minetest.env:get_node(pos).name
+        local source_id = getNumberFromName(source_name) or 0 
+        local coords = {
 
-	   	   {x=pos.x-1,y=pos.y-1,z=pos.z, f=1},   -- vertical drop
-	       {x=pos.x+1,y=pos.y-1,z=pos.z, f=1},   --f= can flow or drop
-		   {x=pos.x,y=pos.y-1,z=pos.z-1, f=1},
-		   {x=pos.x,y=pos.y-1,z=pos.z+1, f=1},	
+            {x=pos.x-1,y=pos.y-1,z=pos.z, f=1},         -- vertical drop
+            {x=pos.x+1,y=pos.y-1,z=pos.z, f=1},         -- f= can flow or drop
+            {x=pos.x,y=pos.y-1,z=pos.z-1, f=1},
+            {x=pos.x,y=pos.y-1,z=pos.z+1, f=1},
 
-	   	   {x=pos.x-1,y=pos.y,z=pos.z,h=1, f=1, wi=1}, -- h=horisontal flow
-	       {x=pos.x+1,y=pos.y,z=pos.z,h=1, f=1, wi=1}, -- wi= standard water infect
-		   {x=pos.x,y=pos.y,z=pos.z-1,h=1, f=1, wi=1},
-		   {x=pos.x,y=pos.y,z=pos.z+1,h=1, f=1, wi=1},	
+            {x=pos.x-1,y=pos.y,z=pos.z,h=1, f=1, wi=1}, -- h=horisontal flow
+            {x=pos.x+1,y=pos.y,z=pos.z,h=1, f=1, wi=1}, -- wi= standard water infect
+            {x=pos.x,y=pos.y,z=pos.z-1,h=1, f=1, wi=1},
+            {x=pos.x,y=pos.y,z=pos.z+1,h=1, f=1, wi=1},	
 
-	   	   {x=pos.x,y=pos.y+1,z=pos.z, wi=1, u=1},   -- look up
+            {x=pos.x,y=pos.y+1,z=pos.z, wi=1, u=1},   -- look up
         }
         local can = 0;
         local can_water = 1;
-    	for i = 1,9 do
-	      	local name = minetest.env:get_node(coords[i]).name
+        --local high_nearby = 0;
+        -- step1: calculate possibility of flow with volumes
+        for i = 1,9 do
+            local name = minetest.env:get_node(coords[i]).name
             local target_id = getNumberFromName(name)
 dPrint("test nei "..name ..' = '.. (target_id or 'NO'))
-	       	if coords[i].f and name == "air" then 
+            if coords[i].f and name == "air" then 
                 coords[i].v = waterplus.finite_water_max_id 
                 coords[i].t = 0
                 can = 1
             elseif name=="default:water_flowing" then
-		        minetest.env:set_node(coords[i],{name = "waterplus:finite_10"})
+                minetest.env:set_node(coords[i],{name = "waterplus:finite_10"})
+                --high_nearby = math.max(high_nearby, 10);
             elseif coords[i].wi and name=="default:water_source" and source_id<waterplus.finite_water_max_id then
 dPrint('convert up='..(coords[i].u or '')..' me=' .. source_id)
-		        minetest.env:set_node(coords[i],{name = "waterplus:finite_"..waterplus.finite_water_max_id})
+                minetest.env:set_node(coords[i],{name = waterplus.finite_water_max_name})
+                --high_nearby = waterplus.finite_water_max_id
             elseif target_id == nil then 
             elseif coords[i].f and target_id >= 1 then 
                 --coords[i].v = waterplus.finite_water_steps - target_id
@@ -138,8 +141,8 @@ dPrint('convert up='..(coords[i].u or '')..' me=' .. source_id)
                 coords[i].o = target_id --original
                 if coords[i].h and pressure < 1 then
                     if coords[i].t < source_id then
-                        can = 1
                         coords[i].v = source_id - target_id
+                        can = 1
                     end
                 else
                     coords[i].v = waterplus.finite_water_max_id - target_id
@@ -147,39 +150,47 @@ dPrint('convert up='..(coords[i].u or '')..' me=' .. source_id)
                 end
 dPrint('test water ' .. (coords[i].wi or 'nwi') .. ' t=' .. target_id)
                 if coords[i].wi and (target_id < waterplus.finite_water_max_id or name == "air") then 
+                    -- do not convert to standard water if flow possible
                     can_water = 0
-dPrint('cant water' .. target_id)
+dPrint('cant water ' .. target_id)
                 end
+                --high_nearby = math.max(high_nearby, target_id);
             end
     	end
-        --local flowed
 
--- twice: for drop then flow
+-- step2: perform flow and drop  twice: first for drop then flow if something rest
         for pass=0,1 do
-          while can>0 and source_id do 
+          while can>0 and source_id>0 do 
             local flowed = 0
-    	    for i = 1+(pass*4),4+(pass*4) do
+            for i = 1+(pass*4),4+(pass*4) do
                 local min = 0
+                --print('testpress ' .. (coords[i].h or 'vertical') .. ' p='.. pressure)
                 if coords[i].h and pressure < 1 then 
-                    min = coords[i].t 
+                    min = coords[i].t or 0
+                    -- trick: flow more if have higher nearby watre level: bad idea for now
+                    --if high_nearby > source_id then 
+                        --min = math.ceil((high_nearby + source_id + min)/3)
+                        --print('cheat min='..min ..' h=' .. high_nearby .. ' s='.. source_id)
+                    --end
                     if not min or min < 1 then min = 1 end
                 end
-                
+--print ('flowto '.. (coords[i].v or'NO') .. ' s='.. source_id .. ' min='.. min)                
+                -- perform one-level flow 
                 if coords[i].v and coords[i].v > 0 and source_id > min then 
                     coords[i].v = coords[i].v - 1
                     source_id = source_id - 1
                     coords[i].a = 1     -- (coords[i].a or 0) + 1
                     coords[i].t = coords[i].t + 1
                     flowed = 1
-dPrint ('flow v=' .. coords[i].v ..' t='.. coords[i].t .. ' s='..source_id)
-                    if source_id <= 1 then break end
+dPrint ('flow v=' .. coords[i].v ..' t='.. coords[i].t .. ' s='..source_id.. ' min='..min)
+                    if source_id < 1 then break end
                 end
             end 
             if source_id < 1 or flowed < 1 then break end
 dPrint ('res flv='..flowed .. ' sid='..source_id)
           end
         end
-        for i = 1,8 do
+        for i = 1,9 do
             if coords[i].a and coords[i].a ~= coords[i].t then 
 dPrint ('repl '..(coords[i].o or 'air') ..' to' .. coords[i].t)
 		        minetest.env:set_node(coords[i],{name = "waterplus:finite_"..coords[i].t})
